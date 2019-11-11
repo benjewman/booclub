b2720f6ba27f80ea770cfb95fb989ccf990e558c
benjewman <bennyadave@gmail.com>
2019-11-11 15:23:57 -0600

    show and index for author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,18 @@
 class AuthorsController < ApplicationController
+
+    def index
+        @authors = Author.all
+    end
+    
+    def show
+        find_author
+        @books = @author.books
+    end
+
+
+    private
+        def find_author
+            @author = Author.find(params[:id])
+        end
+
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,15 @@ class BooksController < ApplicationController
     def index
         @books = Book.all
     end
+
+    def show
+        find_book
+        @author = @book.author
+    end
+
+
+    private
+        def find_book
+            @book = Book.find(params[:id])
+        end
 end
