7310e559b7e3210efa4c5892b3d9f253db8105a2
benjewman <bennyadave@gmail.com>
2019-11-12 11:39:20 -0600

    cleaner summary form still in progress

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -1,30 +1,18 @@
 class SummariesController < ApplicationController
 
     def create
-        # raise params.inspect
-        @book = Book.find(params[:book_id])
-        @summary = @book.summaries.build(summary_params)
-        # @summary.book = @book
+        @summary = Summary.new(summary_params)
+
         if @summary.save
-            puts "AnyTHING"
-            redirect_to book_path(@book)
+            redirect_to book_path(@summary.book)
         else
             redirect_to books_path
         end
-        # if @summary.save
-        #     redirect_to book_path(@book)
-        # else
-        #     flash[:notice] = "Unable to Create Summary"
-        #     redirect_to books_path
-        # end
     end
 
     private
         def summary_params
-            params.require(:summary).permit(:title, :content, :user_id)
+            params.require(:summary).permit(:title, :content, :user_id, :book_id)
         end
 
-        # def book_id_params
-        #     params[:book_id]
-        # end 
 end
