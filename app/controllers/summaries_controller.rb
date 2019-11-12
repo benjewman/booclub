db1a6590f0073c26575a53ffd1a1fb2db7c292a9
benjewman <bennyadave@gmail.com>
2019-11-12 12:11:07 -0600

    added flash notice to summary creation

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -1,18 +1,20 @@
 class SummariesController < ApplicationController
 
     def create
-        @summary = Summary.new(summary_params)
+        @book = Book.find(params[:book_id])
+        @summary = @book.summaries.build(summary_params)
 
         if @summary.save
-            redirect_to book_path(@summary.book)
+            redirect_to book_path(@book)
         else
-            redirect_to books_path
+            flash[:notice] = "Summary couldn't save"
+            redirect_to book_path(@book)
         end
     end
 
     private
         def summary_params
-            params.require(:summary).permit(:title, :content, :user_id, :book_id)
+            params.require(:summary).permit(:title, :content, :user_id)
         end
 
 end
