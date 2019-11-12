f110a747b563c37ba28beceef8ee6d7f317496df
benjewman <bennyadave@gmail.com>
2019-11-12 14:57:27 -0600

    comment forms send to comments controller without creating

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+
+    def create
+        puts "WE GOT TO COMMENTS#CREATE"
+        @book = Book.find(params[:book_id])
+        @comment = Comment.create(comment_params)
+        if @comment.save
+            redirect_to book_path(@book)
+        else
+            # change line below to add flash and send show
+            redirect_to books_path
+        end
+    end
+
+    private
+    
+    def comment_params
+        params.require(:comment).permit(:content, :summary_id, :user_id)
+    end
+end
