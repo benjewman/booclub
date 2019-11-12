2d273b9078c223d3c3860fc96d778e34f0cbc8c6
github NikolaCzv <github nikola.raicic@hotmail.com>
2019-11-12 16:56:10 -0600

    added authorize method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
-class UsersController < ApplicationController 
+class UsersController < ApplicationController
+    before_action :authorize!, only: [:index, :show, :edit, :update]
+
     def show
-        find_user
+        if id_matches_current_user?(params[:id])
+            find_user
+            render :show
+          else
+            flash[:notice] = "You must be logged in to proceed!"
+            redirect_to user_path(session[:user_id])
+        end
     end
 
     def new
